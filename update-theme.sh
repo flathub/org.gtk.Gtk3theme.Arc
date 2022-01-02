a1577cf9e9a668fbfd04970dfc6aabb5721bc928
Patrick Griffis <pgriffis@igalia.com>
2022-01-02 14:31:49 -0600

    Add flathub.json

--- update-theme.sh
+++ update-theme.sh
@@ -5,15 +5,18 @@ set -euf -o pipefail
 declare -A variants=(
   ['-Darker']='"-Dvariants=darker"'
   ['-Dark']='"-Dvariants=dark"'
+  ['-Lighter']='"-Dvariants=lighter"'
   ['-solid']='"-Dvariants=light", "-Dtransparency=false"'
   ['-Darker-solid']='"-Dvariants=darker", "-Dtransparency=false"'
   ['-Dark-solid']='"-Dvariants=dark", "-Dtransparency=false"'
+  ['-Lighter-solid']='"-Dvariants=lighter", "-Dtransparency=false"'
 )
 
 for variant in "${!variants[@]}"; do
   outdir="../org.gtk.Gtk3theme.Arc$variant/"
   sed -e "s/Arc/Arc$variant/g" -e "s/@CONFIG_OPTS@/${variants[$variant]}/" 'org.gtk.Gtk3theme.Arc.json.in' > "$outdir/org.gtk.Gtk3theme.Arc$variant.json"
   sed -e "s/Arc/Arc$variant/g" 'org.gtk.Gtk3theme.Arc.appdata.xml' > "$outdir/org.gtk.Gtk3theme.Arc$variant.appdata.xml"
+  git commit -am 'Add flathub.json'
 done
 
 sed -e 's/@CONFIG_OPTS@/"-Dvariants=light"/' 'org.gtk.Gtk3theme.Arc.json.in' > 'org.gtk.Gtk3theme.Arc.json'
